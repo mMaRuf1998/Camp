cd708230c302fcd780784c89e176ffeb2ffeee14
Maruf Ahmed <marufshmed@gmail.com>
2024-08-24 23:38:55 +0600

    Added and Loaded Data Indexes

---
+++ models/campground.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+
+const Campgroundschema = new Schema(
+    {
+        title: String,
+        price: String,
+        description: String,
+        location: String
+    }
+);
+
+
+module.exports = mongoose.model('Campground', Campgroundschema);
