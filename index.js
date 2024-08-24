16220c09dd93be24f91953ac077aa41ec908fe9b
Maruf Ahmed <marufshmed@gmail.com>
2024-08-25 01:56:13 +0600

    CRUD Operations Added

---
+++ index.js
@@ -0,0 +1,90 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose')
+const Campground = require('./models/campground');
+const methodOverride = require('method-override');
+mongoose.connect('mongodb://localhost:27017/campdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error"));
+db.once("open", () => {
+    console.log('Database connected');
+});
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render("campgrounds/new");
+
+})
+
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground });
+
+})
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campgrounds = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campgrounds._id}`);
+})
+app.post('/campgrounds', async (req, res) => {
+
+
+    const campgrounds = new Campground(req.body.campground);
+    await campgrounds.save();
+    res.redirect(`/campgrounds/${campgrounds._id}`)
+
+
+})
+
+
+app.delete('/campgrounds/:id', async (req, res) => {
+
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+
+
+})
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/show", { campground });
+})
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Serving on Port 3000')
+})
+
+/*
+    name 
+    title
+
+*/
