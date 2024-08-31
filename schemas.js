96c383996a0449f850408a94de2482bb36e22f3b
Maruf Ahmed <marufshmed@gmail.com>
2024-08-31 12:39:50 +0600

    Validation of reviews - Joi - Bootstrap

--- schemas.js
+++ schemas.js
@@ -16,9 +16,9 @@ module.exports.campgroundSchema = campgroundSchema;
 
 const reviewSchema = Joi.object({
     review: Joi.object({
-        rating: Joi.number().required(),
+        rating: Joi.number().required().min(1).max(5),
         body: Joi.string().required()
-    })
-}).required();
+    }).required()
+})
 
-module.exports.reviewSchema = reviewSchema
+module.exports.reviewSchema = reviewSchema;
