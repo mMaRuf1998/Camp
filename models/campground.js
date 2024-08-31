e83862e5c87d445af3386a392dcce661c32e8548
Maruf Ahmed <marufshmed@gmail.com>
2024-08-31 11:45:44 +0600

    Review model added

--- models/campground.js
+++ models/campground.js
@@ -8,7 +8,11 @@ const Campgroundschema = new Schema(
         image: String,
         price: Number,
         description: String,
-        location: String
+        location: String,
+        reviews: [{
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }]
     }
 );
 
