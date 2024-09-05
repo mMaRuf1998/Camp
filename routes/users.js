9ed1f80e8e103d839e1ab6b5f0bb4a2a855cecee
Maruf Ahmed <marufshmed@gmail.com>
2024-09-05 20:11:02 +0600

    Added Registration Forms

--- routes/users.js
+++ routes/users.js
@@ -5,3 +5,9 @@ const User = require("../models/user")
 router.get("/register" , (req,res)=>{
     res.render("users/register") ; 
 })
+
+router.post("/register" , async(req,res)=>{
+    res.send(req.body) ;
+})
+
+module.exports = router
