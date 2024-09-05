650457782f73c5789e4742d020a364fe87347aee
Maruf Ahmed <marufshmed@gmail.com>
2024-09-05 21:37:16 +0600

    Added Login

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,37 @@
 const express= require('express') ; 
 const router = express.Router() ;
 const User = require("../models/user")
+const catchAsync = require('../utils/wrapAround');
+const passport = require("passport")
 
 router.get("/register" , (req,res)=>{
     res.render("users/register") ; 
 })
 
-router.post("/register" , async(req,res)=>{
-    res.send(req.body) ;
+router.post("/register" , catchAsync(async(req,res)=>{
+    //res.send(req.body) ;
+    try{
+    const {email, username,password } = req.body ;
+    const user = new User({email,username}) ;
+    const registeredUser = await User.register(user,password) ;
+    req.flash("success","Welcome to YelpCamp") ;
+    res.redirect("/campgrounds")
+}
+    catch(e){
+        req.flash("error", e.message) 
+        res.redirect("/register")
+    }
+    console.log(registeredUser) ;
+    
+}))
+
+router.get("/login" , (req,res) => {
+    res.render("users/login")
+})
+
+router.post("/login" ,  passport.authenticate('local',{failureFlash:true ,failureRedirect:'/login'}) , (req,res) => {
+    req.flash("success" , "Welcome Back ! ") ;
+    res.redirect("/campgrounds")
 })
 
 module.exports = router
