25616df8c6a90bd5294fc6b5e4182d231819b5f5
Maruf Ahmed <marufshmed@gmail.com>
2024-09-08 20:15:39 +0600

    Seed added for author

--- seeds/seedapp.js
+++ seeds/seedapp.js
@@ -25,11 +25,13 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '66dd9216464e744ce886dcd4' ,
             title: `${sample(descriptors)},${sample(places)}`,
             location: `${cities[random1000].city},${cities[random1000].state}`,
             image: 'https://picsum.photos/750/500',
             description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque officia labore magni recusandae nobis quisquam cumque asperiores, temporibus magnam ipsam velit. Repellendus dicta velit inventore saepe cumque totam dolorem soluta?",
             price: price,
+            
         });
 
         await camp.save();
