803b28d0a3f02c6aeecc21a4f28c687845e6187f
Maruf Ahmed <marufshmed@gmail.com>
2024-09-17 14:56:58 +0600

    Image Uploading to Cloudinary

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -15,8 +15,11 @@ module.exports.renderNewForm =  (req, res) => {
 
 
 module.exports.createCampground = async (req, res, next) => {
+    
     const campground = new Campground(req.body.campground);
+    campground.images = req.files.map(f=>({url:f.path , filename:f.filename}))
     campground.author = req.user._id 
+    console.log(campground)
     await campground.save();
     req.flash('success', 'Successfully added a new Campground !');
     res.redirect(`/campgrounds/${campground._id}`)
--- models/campground.js
+++ models/campground.js
@@ -6,7 +6,12 @@ const User = require("./user")
 const Campgroundschema = new Schema(
     {
         title: String,
-        image: String,
+        images: [
+            {
+                url : String  ,
+                filename : String
+            }
+        ] ,
         price: Number,
         description: String,
         location: String,
