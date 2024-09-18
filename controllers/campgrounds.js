f4eea8ff6117daaa8f5fa9f77d0953547abf9b41
Maruf Ahmed <marufshmed@gmail.com>
2024-09-18 13:43:02 +0600

    File deletion from cloudinery

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,5 @@
 const Campground = require('../models/campground');
-
+const {cloudinary} = require("../cloudinary")
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -66,6 +66,9 @@ module.exports.updateCampground = async (req, res) => {
     await campgrounds.save() ;
 
     if(req.body.deleteImages){
+        for(let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename) ;
+        }
        await campgrounds.updateOne({$pull: {images : {filename:{$in : req.body.deleteImages }}}})
 
     }
