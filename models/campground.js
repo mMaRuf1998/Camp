7aca18a3ca65c748e816da7f43a1202bfb168a83
Maruf Ahmed <marufshmed@gmail.com>
2024-09-18 13:55:09 +0600

    Virtual Schema added in Images

--- models/campground.js
+++ models/campground.js
@@ -3,15 +3,19 @@ const Review = require('./reviews')
 const Schema = mongoose.Schema;
 const User = require("./user")
 
+const ImageSchema = new Schema({
+    url : String ,
+    filename : String 
+}) ;
+
+ImageSchema.virtual('thumbnail').get(function(){
+    return this.url.replace('/upload','/upload/w_300')
+})
+
 const Campgroundschema = new Schema(
     {
         title: String,
-        images: [
-            {
-                url : String  ,
-                filename : String
-            }
-        ] ,
+        images: [ImageSchema] , 
         price: Number,
         description: String,
         location: String,
