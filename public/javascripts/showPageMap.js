a749b40eb9b0986500c08335959df6543b2d0671
Maruf Ahmed <marufshmed@gmail.com>
2024-09-24 23:57:35 +0600

    ShowpageMap for show page

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,10 +2,12 @@
 mapboxgl.accessToken = mapToken ;  
 const map = new mapboxgl.Map({
 	container: 'map', // container ID
-	style: 'mapbox://styles/mapbox/streets-v11', // style URL
+	style: 'mapbox://styles/mapbox/dark-v11', // style URL
 	center: campground.geometry.coordinates, // starting position [lng, lat]
 	zoom: 9, // starting zoom
 });
+
+map.addControl(new mapboxgl.NavigationControl()) ;
 
 
 const marker = new mapboxgl.Marker()
