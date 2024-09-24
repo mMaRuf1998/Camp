c15c59b666a72f4e32cc3e66d6584e5bacb16fa6
Maruf Ahmed <marufshmed@gmail.com>
2024-09-24 23:56:12 +0600

    Added security helmet and sanitizers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-if(process.env.NODE_ENV !== "production"){
-    require('dotenv').config() ;
-}
+
+require('dotenv').config() ;
+
 
 console.log(process.env.secret) ; 
 
@@ -21,6 +21,8 @@ const User = require('./models/user')
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 const userRoutes = require("./routes/users");
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet") 
 
 mongoose.connect('mongodb://localhost:27017/campdb', {
 
@@ -39,18 +41,95 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 const sessionConfig = {
+    name : "YelpSession" , 
     secret: "thisissecret",
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        //secure:true ,
         expires: Date.now() + 1000 * 3600 * 24 * 7,
         maxAge: 1000 * 3600 * 24 * 7
     }
 }
+
+ 
+
+
 app.use(session(sessionConfig));
 
 app.use(flash());
+app.use(helmet()) ;
+ 
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+]; 
+const fontSrcUrls = [];
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: [],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", "blob:"],
+        objectSrc: [],
+        imgSrc: [
+            "'self'",
+            "blob:",
+            "data:",
+            "https://res.cloudinary.com/dl845kosw/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+            "https://images.unsplash.com/",
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls],
+    },
+})) ;
+
+
+
+
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+
 app.use(passport.session()) ;
 app.use(passport.initialize()) ;
 
@@ -74,12 +153,15 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
+app.use(mongoSanitize())
 
 app.use("/", userRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
 
+app.get("/", (req,res) => {
+    res.render('home')
+})
 
 
 app.all('*', (req, res, next) => {
